b274794ad0ed475dfffe8e1c829e2576060c9f92
fgg140704 <129783388+fgg140704@users.noreply.github.com>
2023-04-14 11:10:54 +0300

    Create knock_knock.py

---
+++ knock_knock.py
@@ -0,0 +1,12 @@
+def knock_knock():
+    print("Knock, knock.")
+    input()
+    print("Who's there?")
+    input()
+    print("Boo.")
+    input()
+    print("Boo who?")
+    input()
+    print("Don't cry, it's just a joke!")
+    
+knock_knock()
